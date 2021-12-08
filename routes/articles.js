d27743fa6b4934b0e7a578d6f9206ffcb5aae4d1
stolentino <stotino2000@yahoo.com>
2021-12-07 16:05:16 -0800

    add ability to update and delete an article

--- routes/articles.js
+++ routes/articles.js
@@ -35,7 +35,9 @@ router.post('/', asyncHandler(async (req, res) => {
 
 /* Edit article form. */
 router.get("/:id/edit", asyncHandler(async(req, res) => {
-  res.render("articles/edit", { article: {}, title: "Edit Article" });
+  const article = await Article.findByPk(req.params.id);
+  //res.render("articles/edit", { article: {}, title: "Edit Article" });
+  res.render("articles/edit", { article: article, title: "Edit Article" });
 }));
 
 /* GET individual article. */
@@ -48,16 +50,22 @@ router.get("/:id", asyncHandler(async (req, res) => {
 
 /* Update an article. */
 router.post('/:id/edit', asyncHandler(async (req, res) => {
-  res.redirect("/articles/");
+  const article = await Article.findByPk(req.params.id);
+  await article.update(req.body);
+  //res.redirect("/articles/");
+  res.redirect("/articles/" + article.id);
 }));
 
 /* Delete article form. */
 router.get("/:id/delete", asyncHandler(async (req, res) => {
-  res.render("articles/delete", { article: {}, title: "Delete Article" });
+  const article = await Article.findByPk(req.params.id);
+  res.render("articles/delete", { article, title: "Delete Article" });
 }));
 
 /* Delete individual article. */
 router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const article = await Article.findByPk(req.params.id);
+  await article.destroy();
   res.redirect("/articles");
 }));
 
